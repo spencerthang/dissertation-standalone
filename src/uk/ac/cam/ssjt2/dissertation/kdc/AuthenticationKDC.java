1f48ea7aa6841c341868c87c649d0ac1c7fce0d0
Spencer Thang <spencerthang@gmail.com>
2015-11-04 22:23:16 +0000

    Add key store to KDC.

--- src/uk/ac/cam/ssjt2/dissertation/kdc/AuthenticationKDC.java
+++ src/uk/ac/cam/ssjt2/dissertation/kdc/AuthenticationKDC.java
@@ -3,10 +3,13 @@ package uk.ac.cam.ssjt2.dissertation.kdc;
 import uk.ac.cam.ssjt2.dissertation.client.KDCMessageHandler;
 import uk.ac.cam.ssjt2.dissertation.common.MessageBase;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Spencer on 4/11/2015.
@@ -16,10 +19,19 @@ public class AuthenticationKDC implements Runnable {
     private final int m_Port;
     private Thread m_ServerThread = null;
 
+    private Map<Integer, SecretKey> m_KeyStore = new ConcurrentHashMap<>();
     public ArrayList<MessageBase> UnhandledMessages = new ArrayList<MessageBase>();
 
     public AuthenticationKDC(int port) {
         m_Port = port;
+    }
+
+    public void addKey(int nodeId, SecretKey key) {
+        m_KeyStore.put(nodeId, key);
+    }
+
+    public void sign(int nodeId, byte[] bytes) {
+
     }
 
     @Override
@@ -35,4 +47,6 @@ public class AuthenticationKDC implements Runnable {
             System.err.println(e);
         }
     }
+
+
 }

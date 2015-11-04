a8360e803b40fab31879e87e70ae1e7da228a2ae
Spencer Thang <spencerthang@gmail.com>
2015-11-04 13:02:29 +0000

    Add test for sending messages.

--- src/uk/ac/cam/ssjt2/dissertation/tests/AuthenticationClientTest.java
+++ src/uk/ac/cam/ssjt2/dissertation/tests/AuthenticationClientTest.java
@@ -1,7 +1,8 @@
 package uk.ac.cam.ssjt2.dissertation.tests;
 
 import org.junit.Test;
-import uk.ac.cam.ssjt2.dissertation.AuthenticationClient;
+import uk.ac.cam.ssjt2.dissertation.client.AuthenticationClient;
+import uk.ac.cam.ssjt2.dissertation.common.messages.TestMessage;
 
 import java.io.IOException;
 
@@ -21,14 +22,31 @@ public class AuthenticationClientTest {
     @Test
     public void canOpenConnection() throws IOException {
         // Create test server
-        Thread echoServer = new Thread(new TestServer(c_ServerPort, null));
-        echoServer.start();
+        Thread serverThread = new Thread(new TestServer(c_ServerPort, null));
+        serverThread.start();
 
         // Check connection succeeds
         AuthenticationClient client = new AuthenticationClient(c_ServerAddress, c_ServerPort);
         assertTrue(client.connect());
     }
 
+    @Test
+    public void canSendTestMessage() throws IOException, InterruptedException {
+        // Create test server
+        TestServer testServer = new TestServer(c_ServerPort, null);
+        Thread serverThread = new Thread(testServer);
+        serverThread.start();
+
+        // Send test message
+        AuthenticationClient client = new AuthenticationClient(c_ServerAddress, c_ServerPort);
+        client.connect();
+        client.sendMessage(new TestMessage());
+
+        // Allow 1 second for processing
+        Thread.sleep(1000);
+        assertTrue(testServer.Messages.size() == 1);
+        assertTrue(testServer.Messages.get(0) instanceof TestMessage);
+    }
 
 
 }

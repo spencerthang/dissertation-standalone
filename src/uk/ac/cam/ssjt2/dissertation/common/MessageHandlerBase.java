f6dc8eb3fbf199580fb932fb9e9a66214c9c6927
Spencer Thang <spencerthang@gmail.com>
2015-11-04 14:28:40 +0000

    Add a new abstract message handler.

---
+++ src/uk/ac/cam/ssjt2/dissertation/common/MessageHandlerBase.java
@@ -0,0 +1,49 @@
+package uk.ac.cam.ssjt2.dissertation.common;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by Spencer on 3/11/2015.
+ */
+public abstract class MessageHandlerBase implements Runnable, AutoCloseable {
+
+    protected final InputStream m_InputStream;
+    protected final OutputStream m_OutputStream;
+    protected final boolean m_LeaveOpen;
+
+    public MessageHandlerBase(InputStream inputStream, OutputStream outputStream) {
+        this(inputStream, outputStream, false);
+    }
+
+    public MessageHandlerBase(InputStream inputStream, OutputStream outputStream, boolean leaveOpen) {
+        m_InputStream = inputStream;
+        m_OutputStream = outputStream;
+        m_LeaveOpen = leaveOpen;
+    }
+
+    @Override
+    public void run() {
+        while(m_InputStream != null) {
+            try {
+                handleMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        if(m_InputStream != null && !m_LeaveOpen) {
+            m_InputStream.close();
+        }
+
+        if(m_OutputStream != null && !m_LeaveOpen) {
+            m_OutputStream.close();
+        }
+    }
+
+    public abstract MessageBase handleMessage() throws IOException;
+}

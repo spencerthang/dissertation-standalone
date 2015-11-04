e37cd3df026436af5abbe85079fa3e90442c5107
Spencer Thang <spencerthang@gmail.com>
2015-11-04 04:05:10 +0000

    Initialize JUnit, add blank test.

---
+++ src/uk/ac/cam/ssjt2/dissertation/tests/AuthenticationClientTest.java
@@ -0,0 +1,15 @@
+package uk.ac.cam.ssjt2.dissertation.tests;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthenticationClientTest {
+
+    @Test
+    public void canStartClient() {
+
+    }
+
+}

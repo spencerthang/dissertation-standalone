8745c79f96f7f6a33479591825a5fcab198c0d61
Spencer Thang <spencerthang@gmail.com>
2015-11-04 13:03:39 +0000

    Implement message sending.

--- src/uk/ac/cam/ssjt2/dissertation/client/AuthenticationClient.java
+++ src/uk/ac/cam/ssjt2/dissertation/client/AuthenticationClient.java
@@ -1,6 +1,7 @@
-package uk.ac.cam.ssjt2.dissertation;
+package uk.ac.cam.ssjt2.dissertation.client;
 
 import uk.ac.cam.ssjt2.dissertation.common.AuthenticationProtocol;
+import uk.ac.cam.ssjt2.dissertation.common.MessageBase;
 
 import java.io.*;
 import java.net.Socket;
@@ -22,6 +23,10 @@ public class AuthenticationClient implements AutoCloseable {
     public boolean connect() throws IOException {
         m_Client = new Socket(m_ServerAddress, m_ServerPort);
         return m_Client != null && m_Client.isConnected();
+    }
+
+    public void sendMessage(MessageBase message) throws IOException {
+        m_Client.getOutputStream().write(message.getBytes());
     }
 
     @Override

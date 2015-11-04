d0052afd725268b4abac82918e1510d0e8023435
Spencer Thang <spencerthang@gmail.com>
2015-11-04 12:37:13 +0000

    Authentication client closable, isolate connect.

--- src/uk/ac/cam/ssjt2/dissertation/AuthenticationClient.java
+++ src/uk/ac/cam/ssjt2/dissertation/AuthenticationClient.java
@@ -1,28 +1,33 @@
 package uk.ac.cam.ssjt2.dissertation;
 
+import uk.ac.cam.ssjt2.dissertation.common.AuthenticationProtocol;
+
 import java.io.*;
 import java.net.Socket;
 
 /**
  * Created by Spencer on 1/11/2015.
  */
-public class AuthenticationClient {
+public class AuthenticationClient implements AutoCloseable {
 
     private String m_ServerAddress;
     private int m_ServerPort;
+    private Socket m_Client = null;
 
     public AuthenticationClient(String serverAddress, int serverPort) {
         m_ServerAddress = serverAddress;
         m_ServerPort = serverPort;
     }
 
-    public String testConnection() throws IOException {
-        try(final Socket s = new Socket(m_ServerAddress, m_ServerPort)) {
-            try(BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                PrintWriter out = new PrintWriter(s.getOutputStream(), true)) {
-                out.println("test");
-                return in.readLine();
-            }
+    public boolean connect() throws IOException {
+        m_Client = new Socket(m_ServerAddress, m_ServerPort);
+        return m_Client != null && m_Client.isConnected();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if(m_Client != null) {
+            m_Client.close();
         }
     }
 }

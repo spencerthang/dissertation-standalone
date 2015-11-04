d104eaf37f483c5b07088ea2e5e42c3f8a22cee1
Spencer Thang <spencerthang@gmail.com>
2015-11-04 04:11:32 +0000

    Authentication client constructor.

--- src/uk/ac/cam/ssjt2/dissertation/AuthenticationClient.java
+++ src/uk/ac/cam/ssjt2/dissertation/AuthenticationClient.java
@@ -4,5 +4,13 @@ package uk.ac.cam.ssjt2.dissertation;
  * Created by Spencer on 1/11/2015.
  */
 public class AuthenticationClient {
-    
+
+    private String m_ServerAddress;
+    private int m_ServerPort;
+
+    public AuthenticationClient(String c_ServerAddress, int c_ServerPort) {
+        m_ServerAddress = c_ServerAddress;
+        m_ServerPort = c_ServerPort;
+    }
+
 }
--- src/uk/ac/cam/ssjt2/dissertation/tests/AuthenticationClientTest.java
+++ src/uk/ac/cam/ssjt2/dissertation/tests/AuthenticationClientTest.java
@@ -1,15 +1,19 @@
 package uk.ac.cam.ssjt2.dissertation.tests;
 
-import junit.framework.TestCase;
 import org.junit.Test;
+import uk.ac.cam.ssjt2.dissertation.AuthenticationClient;
 
 import static org.junit.Assert.*;
 
 public class AuthenticationClientTest {
 
-    @Test
-    public void canStartClient() {
+    public static final String c_ServerAddress = "127.0.0.1";
+    public static final int c_ServerPort = 5000;
 
+    @Test
+    public void canCreateNewClient() {
+        AuthenticationClient client = new AuthenticationClient(c_ServerAddress, c_ServerPort);
+        assertNotNull(client);
     }
 
 }

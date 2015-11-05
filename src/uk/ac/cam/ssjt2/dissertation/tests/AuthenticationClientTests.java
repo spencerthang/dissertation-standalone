b52a39ab408aa8988eef3282f95c659f3881efcd
Spencer Thang <spencerthang@gmail.com>
2015-11-05 00:43:54 +0000

    Remove obsolete tests.

--- src/uk/ac/cam/ssjt2/dissertation/tests/AuthenticationClientTests.java
+++ src/uk/ac/cam/ssjt2/dissertation/tests/AuthenticationClientTests.java
@@ -3,10 +3,8 @@ package uk.ac.cam.ssjt2.dissertation.tests;
 import org.junit.Test;
 import uk.ac.cam.ssjt2.dissertation.client.AuthenticationClient;
 import uk.ac.cam.ssjt2.dissertation.common.CipherTools;
-import uk.ac.cam.ssjt2.dissertation.common.messages.TestMessage;
 
 import javax.crypto.SecretKey;
-import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 import static org.junit.Assert.assertNotNull;
@@ -15,8 +13,8 @@ import static org.junit.Assert.assertTrue;
 public class AuthenticationClientTests {
 
     public static final String c_ServerAddress = "127.0.0.1";
-    public static int m_ServerPort = 5000;
-    public static final int m_ClientId = 1;
+    public static int c_ServerPort = 5000;
+    public static final int c_ClientId = 1;
     public final SecretKey m_ClientKey;
 
     public AuthenticationClientTests() throws NoSuchAlgorithmException {
@@ -24,64 +22,14 @@ public class AuthenticationClientTests {
     }
 
     private int getServerPort() {
-        m_ServerPort++;
-        return m_ServerPort;
+        c_ServerPort++;
+        return c_ServerPort;
     }
 
     @Test
     public void canCreateNewClient() {
-        AuthenticationClient client = new AuthenticationClient(m_ClientId, m_ClientKey);
+        AuthenticationClient client = new AuthenticationClient(c_ClientId, m_ClientKey);
         assertNotNull(client);
-    }
-
-    @Test
-    public void canOpenConnection() throws IOException {
-        // Create test server
-        int serverPort = getServerPort();
-        Thread serverThread = new Thread(new TestServer(serverPort, null));
-        serverThread.start();
-
-        // Check connection succeeds
-        AuthenticationClient client = new AuthenticationClient(m_ClientId, m_ClientKey);
-        assertTrue(client.connect(c_ServerAddress, serverPort));
-    }
-
-    @Test
-    public void canSendTestMessage() throws IOException, InterruptedException {
-        // Create test server
-        int serverPort = getServerPort();
-        TestServer testServer = new TestServer(serverPort, null);
-        Thread serverThread = new Thread(testServer);
-        serverThread.start();
-
-        // Send test message
-        AuthenticationClient client = new AuthenticationClient(m_ClientId, m_ClientKey);
-        client.connect(c_ServerAddress, serverPort);
-        client.sendMessage(new TestMessage());
-
-        // Allow 1 second for processing
-        Thread.sleep(1000);
-        assertTrue(testServer.Messages.size() == 1);
-        assertTrue(testServer.Messages.get(0) instanceof TestMessage);
-    }
-
-    @Test
-    public void canReceiveTestMessage() throws IOException, InterruptedException {
-        // Create test server
-        int serverPort = getServerPort();
-        TestServer testServer = new TestServer(serverPort, null);
-        Thread serverThread = new Thread(testServer);
-        serverThread.start();
-
-        // Send test message
-        AuthenticationClient client = new AuthenticationClient(m_ClientId, m_ClientKey);
-        client.connect(c_ServerAddress, serverPort);
-        client.sendMessage(new TestMessage());
-
-        // Await response, allow 2 seconds for processing
-        Thread.sleep(2000);
-        assertTrue(client.UnhandledMessages.size() == 1);
-        assertTrue(client.UnhandledMessages.get(0) instanceof TestMessage);
     }
 
 

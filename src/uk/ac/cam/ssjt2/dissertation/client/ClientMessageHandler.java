d6bbbf0689bb29dc470dfe4f2b6c56e28a3daa21
Spencer Thang <spencerthang@gmail.com>
2015-11-05 01:02:46 +0000

    Add client KDC response handling.

--- src/uk/ac/cam/ssjt2/dissertation/client/ClientMessageHandler.java
+++ src/uk/ac/cam/ssjt2/dissertation/client/ClientMessageHandler.java
@@ -2,7 +2,7 @@ package uk.ac.cam.ssjt2.dissertation.client;
 
 import uk.ac.cam.ssjt2.dissertation.common.AuthenticationProtocol;
 import uk.ac.cam.ssjt2.dissertation.common.MessageHandlerBase;
-import uk.ac.cam.ssjt2.dissertation.common.messages.TestMessage;
+import uk.ac.cam.ssjt2.dissertation.common.messages.KDCResponseMessage;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,10 +28,26 @@ public class ClientMessageHandler extends MessageHandlerBase {
         switch(header) {
             case AuthenticationProtocol.HEADER_TEST:
                 System.out.println("[Client] Received test message.");
-                m_Client.UnhandledMessages.add(new TestMessage().readFromStream(m_InputStream));
+                break;
+            case AuthenticationProtocol.HEADER_KDC_RESPONSE:
+                System.out.println("[Client] Received KDC response message.");
+                try {
+                    KDCResponseMessage.KDCResponse response = KDCResponseMessage.readFromStream(m_InputStream, m_Client.getClientKey());
+
+                    if(response.getClientNonce() != m_Client.getNonce()) {
+                        System.err.println("[Client] KDC response verification failed, nonce mismatch. Expected: " + m_Client.getNonce() + ", expected: " + response.getClientNonce());
+                    } else if(response.getTargetId() != m_Client.getTargetId()) {
+                        System.err.println("[Client] KDC response verification failed, targetId mismatch. Expected: " + m_Client.getTargetId() + ", expected: " + response.getTargetId());
+                    } else {
+                        System.out.println("[Client] KDC response decoded, target: " + response.getTargetId() + ", nonce: " + response.getClientNonce());
+                    }
+                } catch (Exception e) {
+                    System.err.println("[Client] Error occurred while decoding KDC response");
+                    e.printStackTrace();
+                }
                 break;
             default:
-                throw new IllegalArgumentException("Unrecognized message header: " + header);
+                System.err.println("Unrecognized message header: " + header);
         }
     }
 

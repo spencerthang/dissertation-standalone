6c78a902df88f9380bccb9ae963d6e697701f4a2
Spencer Thang <spencerthang@gmail.com>
2015-11-16 05:12:53 +0000

    Add HttpClient to handle posts.

---
+++ src/uk/ac/cam/ssjt2/dissertation/client/HttpClient.java
@@ -0,0 +1,49 @@
+package uk.ac.cam.ssjt2.dissertation.client;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by Spencer on 16/11/2015.
+ */
+public class HttpClient {
+
+    private final URL m_Url;
+
+    public HttpClient(String url) throws IOException {
+        m_Url = new URL(url);
+    }
+
+    public String post(String data) throws IOException {
+        String urlParameters = "data=" + data;
+        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+        int postDataLength = postData.length;
+
+        HttpURLConnection httpConnection = (HttpURLConnection) m_Url.openConnection();
+        httpConnection.setDoOutput(true);
+        httpConnection.setRequestMethod("POST");
+        httpConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        httpConnection.setRequestProperty("charset", "utf8");
+        httpConnection.setUseCaches(false);
+        httpConnection.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+
+        StringBuffer response = new StringBuffer();
+        try(DataOutputStream dataOutputStream = new DataOutputStream(httpConnection.getOutputStream())) {
+            // Send POST data
+            dataOutputStream.write(postData);
+            dataOutputStream.flush();
+        }
+
+        try(BufferedReader in = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()))) {
+            String inputLine;
+            while((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+        }
+
+        return response.toString();
+    }
+
+}

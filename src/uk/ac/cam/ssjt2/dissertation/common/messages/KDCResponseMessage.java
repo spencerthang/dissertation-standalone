c7e73f32ab862010a9729a0da5bf6ebe11f6f00c
Spencer Thang <spencerthang@gmail.com>
2015-11-22 22:13:20 +0000

    Remove logic from dumb data class (KDCResponse).

--- src/uk/ac/cam/ssjt2/dissertation/common/messages/KDCResponseMessage.java
+++ src/uk/ac/cam/ssjt2/dissertation/common/messages/KDCResponseMessage.java
@@ -1,5 +1,6 @@
 package uk.ac.cam.ssjt2.dissertation.common.messages;
 
+import com.sun.prism.PixelFormat;
 import uk.ac.cam.ssjt2.dissertation.common.AuthenticationProtocol;
 import uk.ac.cam.ssjt2.dissertation.common.CipherTools;
 import uk.ac.cam.ssjt2.dissertation.common.Message;
@@ -9,6 +10,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 import java.io.*;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -19,111 +21,35 @@ import java.security.NoSuchAlgorithmException;
  */
 public class KDCResponseMessage extends Message {
 
-    private int m_ClientNonce;
-    private int m_TargetId;
-    private SecretKey m_SessionKey;
+    private final int ClientNonce;
+    private final int TargetId;
+    private final String TargetIV;
+    private final String SessionKey;
+    private final String TargetMessage;
 
-    public KDCResponseMessage(int clientId, SecretKey clientKey, int targetId, SecretKey targetKey, int clientNonce, SecretKey sessionKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+    public KDCResponseMessage(byte header, int clientNonce, int targetId, String targetIV, String sessionKey, String targetMessage) {
         super(AuthenticationProtocol.HEADER_KDC_RESPONSE);
-        CipherTools clientCipher = new CipherTools(clientKey, CipherTools.GenerateIV());
-        CipherTools targetCipher = new CipherTools(targetKey, CipherTools.GenerateIV());
-        byte[] sessionKeyBytes = sessionKey.getEncoded();
-
-        byte[] targetEncryptedMessage;
-        try(ByteArrayOutputStream unencryptedMessage = new ByteArrayOutputStream();
-            DataOutputStream daos = new DataOutputStream(unencryptedMessage)) {
-            daos.writeInt(sessionKeyBytes.length);
-            daos.write(sessionKeyBytes);
-            daos.writeInt(clientId);
-            daos.flush();
-            targetEncryptedMessage = targetCipher.encrypt(unencryptedMessage.toByteArray());
-        }
-
-        byte[] clientEncryptedMessage;
-        try(ByteArrayOutputStream unencryptedMessage = new ByteArrayOutputStream();
-            DataOutputStream daos = new DataOutputStream(unencryptedMessage)) {
-            daos.writeInt(sessionKeyBytes.length);
-            daos.write(sessionKeyBytes);
-            daos.writeInt(targetId);
-            daos.writeInt(clientNonce);
-            daos.writeInt(targetEncryptedMessage.length);
-            daos.write(targetEncryptedMessage);
-            daos.flush();
-            clientEncryptedMessage = clientCipher.encrypt(unencryptedMessage.toByteArray());
-        }
-
-        //m_Buffer.writeInt(clientEncryptedMessage.length);
-        //m_Buffer.write(clientEncryptedMessage);
-    }
-
-    // This function decrypts and extract information from a KDC Response message.
-    // This function does NOT verify the nonce or target ID.
-    public static KDCResponse readFromStream(InputStream inputStream, SecretKey clientKey) throws IOException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
-        DataInputStream dis = new DataInputStream(inputStream);
-        int messageLength = dis.readInt();
-        byte[] encryptedMessage = new byte[messageLength];
-        dis.readFully(encryptedMessage);
-
-        CipherTools clientCipher = new CipherTools(clientKey, CipherTools.GenerateIV());
-        byte[] decrypted = clientCipher.decrypt(encryptedMessage);
-
-        try(DataInputStream decryptedDis = new DataInputStream(new ByteArrayInputStream(decrypted))) {
-            int sessionKeyLength = decryptedDis.readInt();
-            byte[] sessionKeyBytes = new byte[sessionKeyLength];
-            decryptedDis.readFully(sessionKeyBytes);
-            SecretKey sessionKey = new SecretKeySpec(sessionKeyBytes, 0, sessionKeyBytes.length, CipherTools.CipherTransformation);
-
-            int responseTargetId = decryptedDis.readInt();
-            int responseClientNonce = decryptedDis.readInt();
-
-            int targetEncryptedMessageLength = decryptedDis.readInt();
-            byte[] targetEncryptedMessageBytes = new byte[targetEncryptedMessageLength];
-            decryptedDis.readFully(targetEncryptedMessageBytes);
-
-            return new KDCResponse(responseTargetId, responseClientNonce, sessionKey, targetEncryptedMessageBytes);
-        }
-    }
-
-    // Data structure to store useful information from a decoded KDC response message.
-    public static class KDCResponse {
-        private final int m_TargetId;
-        private final int m_ClientNonce;
-        private final SecretKey m_SessionKey;
-        private final byte[] m_TargetEncryptedMessage;
-
-        public KDCResponse(int targetId, int clientNonce, SecretKey sessionKey, byte[] targetEncryptedMessage) {
-            m_TargetId = targetId;
-            m_ClientNonce = clientNonce;
-            m_SessionKey = sessionKey;
-            m_TargetEncryptedMessage = targetEncryptedMessage;
-        }
-
-        public int getTargetId() {
-            return m_TargetId;
-        }
-
-        public int getClientNonce() {
-            return m_ClientNonce;
-        }
-
-        public SecretKey getSessionKey() {
-            return m_SessionKey;
-        }
-
-        public byte[] getTargetEncryptedMessage() {
-            return m_TargetEncryptedMessage;
-        }
+        ClientNonce = clientNonce;
+        TargetId = targetId;
+        TargetIV = targetIV;
+        SessionKey = sessionKey;
+        TargetMessage = targetMessage;
     }
 
     public int getClientNonce() {
-        return m_ClientNonce;
+        return ClientNonce;
     }
 
     public int getTargetId() {
-        return m_TargetId;
+        return TargetId;
     }
 
     public SecretKey getSessionKey() {
-        return m_SessionKey;
+        return new SecretKeySpec(DatatypeConverter.parseBase64Binary(SessionKey), CipherTools.CipherTransformation);
     }
+
+    public byte[] getTargetIv() {
+        return DatatypeConverter.parseBase64Binary(TargetIV);
+    }
+
 }

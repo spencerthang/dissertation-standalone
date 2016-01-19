149c38f6e42256d05104d399b67ae32589bf56c6
Spencer Thang <spencerthang@gmail.com>
2016-01-19 04:19:45 +0000

    Update user QR page with auto-refresh.

--- www/server_user.php
+++ www/server_user.php
@@ -1,11 +1,23 @@
 <?php
 	session_start();
 	$data = array(
-		"Nonce" => session_id(),
-		"ServiceUri" => "http://127.0.0.1/pico/server.php",
-		"ServiceName" => "Test Service"
+		"nonce" => session_id(),
+		"sn" => "Test Service",
+		"sa" => "http://127.0.0.1/pico/server.php",
+		"t" => "SA"
 	);
 ?>
-
+You are <?php echo (isset($_SESSION["authenticated"]) && $_SESSION["authenticated"] === true) ? "authenticated" : "unauthenticated"; ?>.<br />
 Scan the following QR code with the Pico app to continue: <br />
-<img src="qr.php?base64_data=<?php echo base64_encode(json_encode($data)); ?>">
+<img src="qr.php?base64_data=<?php echo base64_encode(json_encode($data)); ?>"><br />
+
+This page will refresh in <span id="countdown"></span> seconds, or <a href='#' onclick='location.reload(true); return false;'>refresh now</a>.
+
+<script language="javascript">
+	(function countdown(remaining) {
+		if(remaining === 0)
+			location.reload(true);
+		document.getElementById('countdown').innerHTML = remaining;
+		setTimeout(function(){ countdown(remaining - 1); }, 1000);
+	})(5);
+</script>

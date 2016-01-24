e808d3242bc5a7bc4c9545e391e9c2869083fbc8
Spencer Thang <spencerthang@gmail.com>
2016-01-24 17:25:46 +0000

    Replace PHP QR code generation with JS.

--- www/server_user.php
+++ www/server_user.php
@@ -9,16 +9,18 @@
 	);
 ?>
 You are <?php echo (isset($_SESSION["authenticated"]) && $_SESSION["authenticated"] === true) ? "authenticated" : "unauthenticated"; ?>.<br />
-Scan the following QR code with the Pico app to continue: <br />
-<img src="qr.php?base64_data=<?php echo base64_encode(json_encode($data)); ?>"><br />
+Scan the following QR code with the Pico app to continue: <br /><br />
+<div id="qrcode"></div><br /><br />
 
 This page will refresh in <span id="countdown"></span> seconds, or <a href='#' onclick='location.reload(true); return false;'>refresh now</a>.
 
-<script language="javascript">
-	(function countdown(remaining) {
-		if(remaining === 0)
-			location.reload(true);
-		document.getElementById('countdown').innerHTML = remaining;
-		setTimeout(function(){ countdown(remaining - 1); }, 1000);
-	})(5);
+<script type="text/javascript" src="qrcode.js"></script>
+<script type="text/javascript">
+(function countdown(remaining) {
+	if(remaining === 0)
+		location.reload(true);
+	document.getElementById('countdown').innerHTML = remaining;
+	setTimeout(function(){ countdown(remaining - 1); }, 1000);
+})(5);
+new QRCode(document.getElementById("qrcode"), '<?php echo json_encode($data); ?>');
 </script>

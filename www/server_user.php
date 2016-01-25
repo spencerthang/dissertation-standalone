aaa9cf74a526b5b9a37803265b6b5c9d0c81bd62
Spencer Thang <spencerthang@gmail.com>
2016-01-25 01:20:06 +0000

    Add auto-reloading of status on page.

--- www/server_user.php
+++ www/server_user.php
@@ -8,7 +8,10 @@
 		"t" => "SA"
 	);
 ?>
-LOGIN STATUS: <?php echo (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] === true) ? "<span style='color:green'>logged in</span>" : "<span style='color:red'>not logged in</span>"; ?>
+<script type="text/javascript" src="qrcode.js"></script>
+<script type="text/javascript" src="jquery-2.2.0.js"></script>
+LOGIN STATUS: <span id="login_status"></span><br />
+UPDATING IN: <span id="timer">loading...</span>
 <br /><br />
 
 <form>
@@ -25,15 +28,40 @@ Scan the following QR code with the Pico app to continue:
 
 <div id="qrcode"></div>
 
-<script type="text/javascript" src="qrcode.js"></script>
 <script type="text/javascript">
-var data = JSON.parse('<?php echo json_encode($data); ?>')
+	var data = JSON.parse('<?php echo json_encode($data); ?>');
 
-function generate_qr() {
-	data["su"] = document.getElementById("username").value;
-	data["sp"] = document.getElementById("password").value;
-	document.getElementById('qrcode').innerHTML = ''
-	new QRCode(document.getElementById("qrcode"), JSON.stringify(data));
-	return false;
-}
+	function generate_qr() {
+		data["su"] = document.getElementById("username").value;
+		data["sp"] = document.getElementById("password").value;
+		document.getElementById('qrcode').innerHTML = '';
+		new QRCode(document.getElementById("qrcode"), JSON.stringify(data));
+		return false;
+	}
+
+	var counter;
+	var interval;
+
+	function setLoadTimer() {
+		counter = 6;
+		interval = setInterval(function() {
+			counter--;
+			// Display 'counter' wherever you want to display it.
+			$('#timer').html(counter + " seconds...");
+			if (counter == 0) {
+				clearInterval(interval);
+				$('#timer').html('loading...');
+				loadStatus();
+			}
+		}, 1000);
+	}
+
+	function loadStatus(){
+		$('#login_status').load('server_user_status.php',function () {
+			$(this).unwrap();
+			setLoadTimer();
+		});
+	}
+
+	loadStatus();
 </script>

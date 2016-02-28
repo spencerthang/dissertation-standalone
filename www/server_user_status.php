a7ece0eedfd25eb0318b1def37b4749c9b825279
Spencer Thang <spencerthang@gmail.com>
2016-02-28 23:50:26 +0000

    Add redirect to wordpress after successful login.

--- www/server_user_status.php
+++ www/server_user_status.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] === true) {
-    echo "<span style='color:green'>logged in</span>";
+    echo "<span style='color:green'>logged in</span><script language='javascript'>window.location = '/wordpress';</script>";
 } else {
     echo "<span style='color:red'>not logged in</span>";
 }
